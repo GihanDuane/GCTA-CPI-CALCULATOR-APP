5506202f2263db1298b4dd74db2704b29dc23e9f
Gihan Duane Gaid <duanegaid.102094@gmail.com>
2024-07-12 00:38:56 +0800

    update this component to recieve and display segmented gcta points along with the tital gcta points

--- src/components/GctaCalculatorPage/index.tsx
+++ src/components/GctaCalculatorPage/index.tsx
@@ -13,13 +13,14 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 // import { Fullscreen } from "@mui/icons-material";
 // import { Button, FormControl } from "@mui/material";
 // import { text } from "stream/consumers";
 import GctaCard from "@components/GctaCard";
 import CpiCard from "@components/CpiCard";
 import CpiAndGctaDetails from "@components/CpiAndGctaDetails";
+import CutOffDatesDropDown from "@components/CutOffDatesDropDown";
+// import { Dayjs } from "dayjs";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -44,8 +45,34 @@ const GctaCalculatorPage: React.FC = ({}) => {
   const [cpiMonths, setCpiMonths] = React.useState<number | null>(null);
   const [cpiYears, setCpiYears] = React.useState<number | null>(null);
 
+  // const [cutoffValue, setCutoffValue] = React.useState<number | null>(null);
+
+  const [totalGctaPoints, setTotalGctaPoints] = React.useState<number>(0);
+  const [gctaPoints20, setGctaPoints20] = React.useState<number>(0);
+  const [gctaPoints23, setGctaPoints23] = React.useState<number>(0);
+  const [gctaPoints25, setGctaPoints25] = React.useState<number>(0);
+  const [gctaPoints30, setGctaPoints30] = React.useState<number>(0);
+
   const handleExpandClick = () => {
     setExpanded(!expanded);
+  };
+
+  // const handleCutoffSelect = (value: number) => {
+  //   setCutoffValue(value);
+  // };
+
+  const handleCalculate = (
+    totalPoints: number,
+    points20: number,
+    points23: number,
+    points25: number,
+    points30: number
+  ) => {
+    setTotalGctaPoints(totalPoints);
+    setGctaPoints20(points20);
+    setGctaPoints23(points23);
+    setGctaPoints25(points25);
+    setGctaPoints30(points30);
   };
 
   return (
@@ -58,14 +85,15 @@ const GctaCalculatorPage: React.FC = ({}) => {
             </Avatar>
           }
           action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
+            <div aria-label="settings">
+              <CutOffDatesDropDown />
+            </div>
           }
           title="CPI and GCTA Calculator"
           style={{ textAlign: "center", color: "black" }}
           // subheader="PDL"
         />
+
         {/* <CardMedia
         component="img"
         height="194"
@@ -81,7 +109,10 @@ const GctaCalculatorPage: React.FC = ({}) => {
             setCpiYears={setCpiYears}
           />
 
-          <GctaCard />
+          <GctaCard
+            // cutOffValue={cutoffValue}
+            onCalculate={handleCalculate}
+          />
         </div>
 
         <CardActions disableSpacing>
@@ -146,6 +177,11 @@ const GctaCalculatorPage: React.FC = ({}) => {
         cpiYears={cpiYears}
         cpiMonths={cpiMonths}
         cpiDays={cpiDays}
+        totalGctaPoints={totalGctaPoints}
+        gctaPoints20={gctaPoints20}
+        gctaPoints23={gctaPoints23}
+        gctaPoints25={gctaPoints25}
+        gctaPoints30={gctaPoints30}
       />
     </div>
   );
