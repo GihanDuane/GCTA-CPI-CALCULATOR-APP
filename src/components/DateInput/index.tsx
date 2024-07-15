797f750c6dafbde3d92121dd85fd314491cee34f
Gihan Duane Gaid <duanegaid.102094@gmail.com>
2024-07-15 17:18:12 +0800

    add error to DateInput component

--- src/components/DateInput/index.tsx
+++ src/components/DateInput/index.tsx
@@ -6,17 +6,24 @@ import dayjs, { Dayjs } from "dayjs";
 interface DateInputProps {
   value: Dayjs | null;
   onChange: (newValue: Dayjs | null) => void;
+  error?: boolean;
 }
 
-const DateInput: React.FC<DateInputProps> = ({ value, onChange }) => {
+const DateInput: React.FC<DateInputProps> = ({ value, onChange, error }) => {
   const adaptedValue = value ? dayjs(value) : null;
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className="pt-2">
         <DatePicker
-          label="Select date"
+          label={error ? "This field is required!" : "Select date"}
           value={adaptedValue}
           onChange={onChange}
+          slotProps={{
+            textField: {
+              error: error,
+              // helperText: error ? "This field is required" : "",
+            },
+          }}
         />
       </div>
     </LocalizationProvider>
